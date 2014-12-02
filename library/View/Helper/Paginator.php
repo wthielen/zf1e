9f09c6362005c8ca975569732f3bde41f872631f
Wouter Thielen <wouter@metroworks.co.jp>
2014-12-02 18:06:14 +0900

    Implemented range in the Paginator
    Also changed it so that it uses the $pageInfo from the arguments,
    instead of from the view. This is better in case there are multiple
    paginators on a page.

--- library/View/Helper/Paginator.php
+++ library/View/Helper/Paginator.php
@@ -18,15 +18,21 @@ class ZFE_View_Helper_Paginator extends Zend_View_Helper_Abstract
      * or show an input field that shows the current page number, but allows the
      * user to enter any page number.
      */
-    public function paginator($options = array())
+    public function paginator($pageInfo, $options = array())
     {
-        $pageInfo = $this->view->pageInfo;
-
+        $maxEntries = 11;
         $baseUrl = $this->view->url();
 
         // Some handy booleans
         $firstPage = $pageInfo['page'] == 1;
         $lastPage = $pageInfo['page'] == $pageInfo['pages'];
+
+        $halfPoint = floor($maxEntries / 2);
+        $start = $pageInfo['page'] > $halfPoint ? $pageInfo['page'] - $halfPoint : 1;
+        $end = $start + $maxEntries - 1;
+
+        if ($end > $pageInfo['pages']) $end = $pageInfo['pages'];
+        if ($end - $start < $maxEntries) $start = max(1, $end - $maxEntries + 1);
 
         $html = '<ul class="pagination">';
 
@@ -42,10 +48,12 @@ class ZFE_View_Helper_Paginator extends Zend_View_Helper_Abstract
             $url = $baseUrl . '?' . http_build_query(array('p' => $pageInfo['page'] - 1));
             $html .= '<a href="' . $url . '" data-page="' . ($pageInfo['page'] - 1) . '">&lt;</a>';
             $html .= '</li>';
+
+            if ($start > 1) $html .= '<li class="disabled"><span>...</span></li>';
         }
 
         // Add the page number links, and make the current one active
-        for($page = 1; $page <= $pageInfo['pages']; $page++) {
+        for($page = $start; $page <= $end; $page++) {
             $url = $baseUrl . '?' . http_build_query(array('p' => $page));
 
             $cls = array();
@@ -59,12 +67,14 @@ class ZFE_View_Helper_Paginator extends Zend_View_Helper_Abstract
         // If not on the last page, show the "go to next page" and "go to last
         // page" links.
         if (!$lastPage) {
+            if ($end < $pageInfo['pages']) $html .= '<li class="disabled"><span>...</span></li>';
+
             $html .= '<li class="next">';
             $url = $baseUrl . '?' . http_build_query(array('p' => $pageInfo['page'] + 1));
             $html .= '<a href="' . $url . '" data-page="' . ($pageInfo['page'] + 1) . '">&gt;</a>';
             $html .= '</li>';
 
-            $html .= '<li class="previous">';
+            $html .= '<li class="last">';
             $url = $baseUrl . '?' . http_build_query(array('p' => $pageInfo['pages']));
             $html .= '<a href="' . $url . '" data-page="' . $pageInfo['pages'] . '">&gt;&gt;</a>';
             $html .= '</li>';
