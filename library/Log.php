0e8d05609109334c4cf187c0b2f8731e6d39954f
Wouter Thielen <wouter@morannon.org>
2014-12-20 23:58:12 +0900

    Added a ZFE_Log library
    Logging is done using the Zend Log application resource. This can be
    configured in application.ini. It will fall-back to error_log if there
    is none configured.
    
    For more information on configuring the Zend_Log resource:
    http://framework.zend.com/manual/1.12/en/zend.log.html

---
+++ library/Log.php
@@ -0,0 +1,37 @@
+<?php
+
+abstract class ZFE_Log
+{
+    private static $logger;
+
+    protected static function getLogger()
+    {
+        if (is_null(self::$logger)) {
+            $front = Zend_Controller_Front::getInstance();
+            $bootstrap = $front->getParam('bootstrap');
+
+            self::$logger = $bootstrap->getResource('Log');
+        }
+
+        return self::$logger;
+    }
+
+    public static function __callStatic($method, $arguments)
+    {
+        if (count($arguments) == 0) return;
+
+        $logger = self::getLogger();
+        $message = $arguments[0];
+
+        if (is_null($logger)) {
+            error_log($message);
+            return;
+        }
+
+        try {
+            call_user_func_array(array($logger, $method), $arguments);
+        } catch (Zend_Log_Exception $e) {
+            error_log($message);
+        }
+    }
+}
