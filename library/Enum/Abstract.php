e02be2f3f451000c5737e398c38c01e5f2674e47
Wouter Thielen <wouter@morannon.org>
2015-04-14 22:27:34 +0900

    Adding an abstract Enum class
    Pretty handy for having your own enumerations, if you miss them from
    C/C++/Java.

---
+++ library/Enum/Abstract.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * An Enum abstract class to create your own Enums
+ *
+ * Example with translated string values:
+ * abstract class XYZ_Enum_Color extends ZFE_Enum_Abstract
+ * {
+ *     const WHITE = 1; // Can be a string too like 'white'
+ *     const BLACK = 2;
+ *     const RED = 3;
+ *     const BLUE = 4;
+ *
+ *     public static str($item)
+ *     {
+ *         $ml = Zend_Registry::get('ZFE_MultiLanguage');
+ *         $ret = parent::str($item);
+ *
+ *         if ($item == self::WHITE) $ret = $ml->_('COLOR_WHITE');
+ *         if ($item == self::BLACK) $ret = $ml->_('COLOR_BLACK');
+ *         // etc...
+ *
+ *         return $ret;
+ *     }
+ * }
+ *
+ * Then you can have: $color = XYZ_Enum_Color::BLACK;
+ * and: echo XYZ_Enum_Color::str($color);
+ *
+ * And to get a list of values, for your select pulldown for example,
+ * call XYZ_Enum_Color::getValues()
+ */
+abstract class ZFE_Enum_Abstract
+{
+    public static function getValues()
+    {
+        $reflection = new ReflectionClass(get_called_class());
+
+        $constants = $reflection->getConstants();
+
+        $ret = array();
+        foreach($constants as $val) $ret[$val] = static::str($val);
+        return $ret;
+    }
+
+    public static function str($item) {
+        return '(unknown)';
+    }
+}
