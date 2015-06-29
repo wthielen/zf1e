4f8fe54fc1730906ab778d3a0f4eef43383d5fd6
Wouter Thielen <wouter@metroworks.co.jp>
2015-06-29 14:19:32 +0900

    Putting getModule stuff back in
    Still useful.

--- library/Application/Cli.php
+++ library/Application/Cli.php
@@ -7,13 +7,24 @@ class ZFE_Application_Cli extends Zend_Application
     public function __construct($environment, $options)
     {
         $this->opts = new Zend_Console_Getopt(array(
+            'module|m=w' => 'Select module',
             'verbose|v' => 'Be verbose',
             'help|h' => 'Show this help text'
         ));
 
+        $module = $this->getModule();
+        if ($module !== 'default') {
+            $options['config'][] = APPLICATION_PATH . '/modules/' . $module . '/configs/module.ini';
+        }
+
         parent::__construct($environment, $options);
 
         Zend_Registry::set('CliApplication', $this);
+    }
+
+    public function getModule()
+    {
+        return $this->opts->module ? $this->opts->module : 'default';
     }
 
     public function run()
